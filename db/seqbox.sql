2041a06eb362a551f82431f8fbd4b59797b8163a
iskounene <lydia.iskounene@etu.u-pec.fr>
2019-06-24 04:37:50 +0200

    add to db

--- db/seqbox.sql
+++ db/seqbox.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  ven. 21 juin 2019 à 10:16
+-- Généré le :  lun. 24 juin 2019 à 02:35
 -- Version du serveur :  5.7.24
 -- Version de PHP :  7.2.14
 
