5a3d245bc64aaaa90b4e8400981568560269a6f9
iskounene <44842872+iskounene@users.noreply.github.com>
2019-07-29 08:50:31 +0200

    add to flask-test

--- src/main.py
+++ src/main.py
@@ -1,25 +1,7 @@
-from flask import Flask, request, redirect, render_template
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__) 
-app.config['DEBUG'] = True
+from app import app, db
+from app.models import User, Post
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:vitrygtr@localhost:3306/seqbox'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-db = SQLAlchemy(app)
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True) 
-    email = db.Column(db.String(75)) 
-    username = db.Column(db.String(50)) 
-    password = db.Column(db.String(100)) 
-
-
-@app.route('/') 
-def home(): 
-    return render_template('home.html', title='Home') 
-    
-if __name__ == '__main__': 
-    app.run()
+@app.shell_context_processor
+def make_shell_context():
+    return {'db' : db, 'User':User, 'Post' : Post}
