4141f6d110558b103b46d2af8667216e7dfc10dd
iskounene <lydia.iskounene@etu.u-pec.fr>
2019-07-30 10:49:58 +0200

    add to flask

---
+++ src/test/mysql-test.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+import mysql.connector
+
+
+app = Flask(__name__)
+
+
+
+
+cnx = mysql.connector.connect(user='oucru', password='vitrygtr',
+                              host='127.0.0.1',
+                              database='seqbox')
+
+
+sql_select_query = """ SELECT id_sample, num_seq FROM `sample` WHERE 1"""
+
+cursor = cnx.cursor()
+
+result = cursor.execute(sql_select_query)
+
+
+records = cursor.fetchall()
+
+@app.route('/') 
+def sample(): 
+    return render_template('sample.html',title='Sample') 
+    
+if __name__ == '__main__': 
+    app.run()
+
+
+
