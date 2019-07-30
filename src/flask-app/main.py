25bb7e487897268880de5dfa49439614b9cfe1b0
iskounene <lydia.iskounene@etu.u-pec.fr>
2019-07-30 11:54:07 +0200

    add to src

---
+++ src/flask-app/main.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__) 
+app.config['DEBUG'] = True
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:vitrygtr@localhost:3306/seqbox'
+app.config['SQLALCHEMY_ECHO'] = True
+
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True) 
+    email = db.Column(db.String(75)) 
+    username = db.Column(db.String(50)) 
+    password = db.Column(db.String(100)) 
+
+
+@app.route('/') 
+def home(): 
+    return render_template('home.html', title='Home') 
+    
+if __name__ == '__main__': 
+    app.run()
---
+++ src/flask-test/config.py
@@ -0,0 +1,11 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL=mysql://root:vitrygtr@localhost/seqbox')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    
